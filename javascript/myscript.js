0a8441272b2db026914c986006da12a2f98b2031
DavideZattra <d.zattra@gmail.com>
2021-09-16 15:31:43 +0200

    implemented push and delete function

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -3,10 +3,30 @@ var app = new Vue(
 		el : '#app',
 
 		data : {
-			toDolist : ['', '', '', ''],
+			toDoList : ['comprare nuove scarpe'],
+
+			listItem : '',
 		},
 
-		methods: {}
+		methods: {
+
+			addListItem : function(){
+
+				if(this.listItem.length>0){
+
+					this.toDoList.push(this.listItem.trim());
+					this.listItem = '';
+
+				}
+				console.log(this.toDoList);
+			},
+
+			removeListItem : function (index){
+				this.toDoList.splice(index,1);
+				console.log(this.toDoList);
+
+			}
+		}
 		
 
 	}
